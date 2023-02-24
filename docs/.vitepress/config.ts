e8d7cd91bd4a6c0d8c7464dfeedbe553f059348e
shibin-cli <shibiny6@gmail.com>
2023-02-24 16:55:04 +0800

    feat: 🎸 init

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: 'Go学习',
+  description: 'Go基础学习',
+  base:'/learn-go/',
+  head: [['meta', { name: 'theme-color', content: '#3c8772' }]],
+  themeConfig: {
+    sidebar: {
+      '/base/': sidebarBase(),
+    },
+    nav: nav(),
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/shibin-cli/learn-go' }
+    ],
+    editLink: {
+      pattern: 'https://github.com/shibin-cli/learn-go/edit/main/docs/:path'
+    }
+  },
+  
+})
+
+function sidebarBase(){
+  return [
+    {
+      text: '理解rpc',
+      collapsed: true,
+      items: [
+        { text: '开始', link: '/base/start.md' },
+        { text: '编码规范', link: '/base/coding-specification.md' },
+      ]
+  }]
+}
+
+function nav() {
+  return [
+    { text: '基础', link: '/base/coding-specification.md', activeMatch: '/base/' },
+  ]
+} 
