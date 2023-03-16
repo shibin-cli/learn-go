311f88e1364ccb3507f55ccd701975e035e9f6a7
shibin-cli <shibiny6@gmail.com>
2023-03-16 10:27:06 +0000

    Deploying to gh-pages from @ shibin-cli/learn-go@d47e2819c2c0819782b653026d4c36de960e7e78 🚀

---
+++ assets/base_gorm_start.md.809dd300.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as n,Q as l}from"./chunks/framework.695d4026.js";const i=JSON.parse('{"title":"GORM","description":"","frontmatter":{},"headers":[],"relativePath":"base/gorm/start.md"}'),p={name:"base/gorm/start.md"},o=l("",23),e=[o];function t(r,c,D,F,y,A){return n(),a("div",null,e)}const g=s(p,[["render",t]]);export{i as __pageData,g as default};
